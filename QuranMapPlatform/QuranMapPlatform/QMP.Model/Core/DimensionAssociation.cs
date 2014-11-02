e671ca8d5a27eedfaaa88cb6527eaa9575caa2f2
Jimmy <jamalitcis@gmail.com>
2014-11-02 10:48:48 -0800

    add model

---
+++ QuranMapPlatform/QuranMapPlatform/QMP.Model/Core/DimensionAssociation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QMP.Model.Core
+{
+    public class DimensionAssociation
+    {
+        public Dimension Source
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public Dimension Target
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
---
+++ QuranMapPlatform/QuranMapPlatform/QMP.Model/Core/FactAssociation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QMP.Model.Core
+{
+    public class FactAssociation
+    {
+        public Fact Source
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public Fact Target
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public DimensionAssociation DimensionAssociation
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
